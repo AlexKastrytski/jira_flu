5551a975da788820ecbc953260a2cc73c14faaa5
AlexKastrytski <akastrytski@hotmail.com>
2024-04-30 14:19:03 +0300

    fdf

--- delete_BD.py
+++ delete_BD.py
@@ -1,9 +1,11 @@
 import influxdb_client, os, time # type: ignore
-from influxdb_client import InfluxDBClient, Point, WritePrecision # type: ignore
+from influxdb_client import InfluxDBClient, Point, WritePrecision, get_list_database # type: ignore
 from influxdb_client.client.write_api import SYNCHRONOUS # type: ignore
 token = 'FKNAyFerai9zrEuwq2VsOFxWSNfidmzJDWiEBTU-iy0Qsrh0fIaXLbJLRga53Whw-rU0n1io1Yk-h2biQPpwIw=='
 org = "A1_IKT"
 url = "http://192.168.16.15:8086"
+influx_host = os.getenv('INFLUX_HOST', 'localhost')
+write_client = InfluxDBClient(host=influx_host)
 write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 db_list = write_client.get_list_database()
 for db in db_list:
